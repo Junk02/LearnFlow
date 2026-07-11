d3ab1e12142853a60dbcdc1610749eee3ecb4aeb
Jun_k01 <kleshchukevichart@gmail.com>
2026-07-11 21:23:57 +0300

    Created script for getting text from pdf
    Implemented get_text_from_pdf.py script and added "byte_of_python.pdf" for testing

---
+++ get_text_from_pdf.py
@@ -0,0 +1,15 @@
+import pdfplumber
+
+def extract_text(pdf_path):
+    full_text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                full_text += text + "\n"
+    return full_text
+
+if __name__ == "__main__":
+    path = "byte_of_python.pdf"
+    text = extract_text(path)
+    print(text)
